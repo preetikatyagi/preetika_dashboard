11f1d1055f67446d518b45043ada372b115d202b
Preetika <preetikatyagi@gmail.com>
2010-06-30 15:35:06 -0700

    Import Screen for Sequencer

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,10 +9,15 @@ class WelcomeController < ApplicationController
 	def demo
 		render :layout => 'demo'
 	end
-	
+	# Added by Preetika Tyagi
 	def dashboard
-		
-	end
-	
+	  @colorSequencer = '#996600'
+	  @sequencer = 'Sequencer'
+    @upload = Upload.new
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @upload }
+    end
+  end
 	
 end
