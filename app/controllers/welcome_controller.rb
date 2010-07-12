6e19ce6a59b62b09c4d4ad0415e3cad529ba3e93
Preetika <preetikatyagi@gmail.com>
2010-07-12 11:21:10 -0700

    Dashboard

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,15 +9,19 @@ class WelcomeController < ApplicationController
 	def demo
 		render :layout => 'demo'
 	end
-	# Added by Preetika Tyagi
+	# Added by Preetika Tyagi - begins
 	def dashboard
 	  @colorSequencer = '#996600'
 	  @sequencer = 'Sequencer'
     @upload = Upload.new
+    @delayed_jobs = DelayedJob.all
+	  @autorefresh = true
+	  @uploadID = '0'
+	  @importID = '0'
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @upload }
+      format.xml  { render :xml => @delayed_jobs }
     end
   end
-	
+	# Added by Preetika Tyagi - ends
 end
